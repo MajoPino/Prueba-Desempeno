74bfad6dd81079bafc07a7c22106951051d2b69d
MajoPino <majo12pino@gmail.com>
2024-06-11 09:46:35 -0500

    Creating all the basic structure of the project

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/* 
+Mariajose Pino Ortega
+Clan Gates
+Tuesday June/11/2024 */
+
+
+// Import SCSS
+import '../scss/styles.scss'
+// Import Bootstrap JS
+import * as bootstrap from 'bootstrap'
+//Import Sweetalert
+import Swal from 'sweetalert2'
